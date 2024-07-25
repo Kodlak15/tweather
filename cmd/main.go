1276b44cef0970cfefabd76d5c8af5cc7bd7ced4
Kodlak15 <stanlcod15@gmail.com>
2024-07-25 15:01:51 -0700

    Set up api requests + create relevant Go types

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-// TODO build CLI + configuration system + (maybe) TUI
+// TODO: build CLI + configuration system + (maybe) TUI
 
 type CurrentWeather struct {
 	Timestamp int32     `json:"dt"`
@@ -23,6 +23,16 @@ type Forecast struct {
 		Weather   []Weather `json:"weather"`
 		Main      Main      `json:"main"`
 	} `json:"list"`
+}
+
+// TODO: should be an option to do this by city/state/country or zip
+// There is also a reverse geocoding functionality that I may or may not want to support
+type Geocoding []struct {
+	Name    string  `json:"name"`
+	Lat     float32 `json:"lat"`
+	Lon     float32 `json:"lon"`
+	Country string  `json:"country"`
+	State   string  `json:"state"`
 }
 
 type Coords struct {
@@ -46,38 +56,53 @@ type Main struct {
 	Humidity  int32   `json:"humidity"`
 }
 
-func getCurrentWeather(key string) {
+func getCurrentWeather(key string) CurrentWeather {
 	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s&units=%s",
 		0.0, 0.0, key, "imperial")
-
-	// TODO check and handle specific error codes
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Request failed:", err)
-		os.Exit(1)
-	}
-	defer response.Body.Close()
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Error reading the response body:", err)
-		os.Exit(1)
-	}
+	body := getApiResponseBody(url)
 
 	var data CurrentWeather
-	if err = json.Unmarshal(body, &data); err != nil {
+	if err := json.Unmarshal(body, &data); err != nil {
 		fmt.Println("Error unmarshalling JSON from response body:", err)
 		os.Exit(1)
 	}
 
 	fmt.Println(data)
+	return data
 }
 
-func getForecast(key string) {
+func getForecast(key string) Forecast {
 	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/forecast?lat=%f&lon=%f&appid=%s&units=%s",
 		0.0, 0.0, key, "imperial")
+	body := getApiResponseBody(url)
 
-	// TODO check and handle specific error codes
+	var data Forecast
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println("Error unmarshalling JSON from response body:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(data)
+	return data
+}
+
+func getGeocoding(key string) Geocoding {
+	url := fmt.Sprintf("http://api.openweathermap.org/geo/1.0/direct?q=%s,%s,%s&limit=%d&appid=%s",
+		"Phoenix", "AZ", "USA", 1, key)
+	body := getApiResponseBody(url)
+
+	var data Geocoding
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println("Error unmarshalling JSON from response body:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(data)
+	return data
+}
+
+func getApiResponseBody(url string) []byte {
+	// TODO: check and handle specific error codes
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Request failed:", err)
@@ -91,18 +116,14 @@ func getForecast(key string) {
 		os.Exit(1)
 	}
 
-	var data Forecast
-	if err = json.Unmarshal(body, &data); err != nil {
-		fmt.Println("Error unmarshalling JSON from response body:", err)
-		os.Exit(1)
-	}
-
-	fmt.Println(data)
+	return body
 }
 
 func main() {
+	// TODO: find better way to take cli arguments
 	key := os.Args[1]
 
 	getCurrentWeather(key)
 	getForecast(key)
+	getGeocoding(key)
 }
